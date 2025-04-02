42fff1dc3817797760b83420195f344075321545
guleixin <18017158729@163.com>
2025-04-02 18:32:53 +0800

    ✨ feat: 修改布局为拉伸

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,16 +2,16 @@
 </script>
 
 <template>
-  <div class="d-flex align-items-start h-100 ">
+  <div class="d-flex align-items-stretch h-100">
     <!-- 垂直导航栏 -->
-    <div class="nav flex-column nav-pills text-bg-dark h-100" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <div class="nav flex-column nav-pills text-bg-dark" id="v-pills-tab" role="tablist" aria-orientation="vertical">
       <router-link to="/" class="nav-link" active-class="active" exact>Home</router-link>
       <router-link to="/cesium" class="nav-link" active-class="active">Cesium</router-link>
       <router-link to="/bootstrap" class="nav-link" active-class="active">Bootstrap</router-link>
     </div>
 
     <!-- 路由内容区域 -->
-    <div class="flex-grow-1 h-100">
+    <div class="flex-grow-1">
       <router-view></router-view>
     </div>
   </div>
