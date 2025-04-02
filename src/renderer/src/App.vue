dd03ada2746ba0de8853a2f8ff118a9f77d11e9c
guleixin <18017158729@163.com>
2025-04-02 10:40:42 +0800

    ✨ feat: 添加侧边导航栏

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,19 +1,17 @@
-<script setup lang="ts">
-import Versions from './components/Versions.vue'
-
-const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+<script setup>
 </script>
 
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <div class="d-flex align-items-start">
+    <!-- 垂直导航栏 -->
+    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <router-link to="/" class="nav-link" active-class="active" exact>Home</router-link>
+      <router-link to="/about" class="nav-link" active-class="active">About</router-link>
+    </div>
+
+    <!-- 路由内容区域 -->
+    <div class="flex-grow-1">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,7 +1,8 @@
 // import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router'
+import './assets/styles.scss'
 
 createApp(App).use(router).mount('#app')
